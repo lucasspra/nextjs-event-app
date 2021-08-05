a085eea59e2240e73f7e13d88e133947b86c0352
Lucas <lucas.souza@ifood.com.br>
2021-08-05 15:14:40 -0300

    add generic head and fix time search

--- components/events/events-search.jsx
+++ components/events/events-search.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
@@ -19,20 +20,15 @@ function EventsSearch(props) {
     <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
-          <label htmlFor="year" ref={yearInputRef}>
-            Year
-          </label>
-          <select id="year">
+          <label htmlFor="year">Year</label>
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
-
         <div className={classes.control}>
-          <label htmlFor="month" ref={monthInputRef}>
-            Month
-          </label>
-          <select id="month">
+          <label htmlFor="month">Month</label>
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
@@ -41,15 +37,14 @@ function EventsSearch(props) {
             <option value="6">June</option>
             <option value="7">July</option>
             <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">Octuber</option>
+            <option value="9">Septemer</option>
+            <option value="10">October</option>
             <option value="11">November</option>
             <option value="12">December</option>
           </select>
         </div>
-
-        <Button>Find events</Button>
       </div>
+      <Button>Find Events</Button>
     </form>
   );
 }
--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from "next/head";
 import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
@@ -17,6 +18,11 @@ function EventDetailPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
+
       <EventSummary title={event.title} />
 
       <EventLogistics
